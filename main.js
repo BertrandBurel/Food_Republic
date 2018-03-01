96ab722e99ec1f1a892221b4fcddfbc1d09a3a34
Martins Alexandre <xeos28@gmail.com>
2018-03-01 13:26:08 +0100

    mise à jour script menu burger

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 var getBtn = document.getElementById("btnBurger");
 var menuBurger = document.getElementById("menuBurger");
+    menuBurger.style.display = "none";
 var barMenu = document.getElementById("barreMenu");
-menuBurger.style.display = "none";
 
 function displayBurger (){
     if(menuBurger.style.display === "none"){
-        menuBurger.style.display = "block";
-        barMenu.style.width = "60vw";
-        barMenu.style.marginLeft ="40vw";
+        menuBurger.style.display = "block"; 
+        document.body.style.paddingLeft= "220px";
     }else if (menuBurger.style.display === "block"){
         menuBurger.style.display = "none";
-        barMenu.style.width = "100vw";
-        barMenu.style.margin = "0px";
+        document.body.style.paddingLeft ="0";
     }
 };
 
 getBtn.addEventListener("click",displayBurger);
+

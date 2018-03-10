3132e5b5f026b7d4318a69b92825f14ac03fa8cc
Martins Alexandre <xeos28@gmail.com>
2018-03-11 00:13:51 +0100

    ajout des menus scroll (mobile et grand ecran, ajout du scroll et fixed sur burger et corection d'affichage en mobile

--- main.js
+++ main.js
@@ -1,31 +1,76 @@
 var getBtn = document.getElementById("btnBurger");
 var menuBurger = document.getElementById("menuBurger");
-    menuBurger.style.display = "none";
+menuBurger.style.display = "none";
+
 var barMenu = document.getElementById("barreMenu");
 
-function displayBurger (){
-    if(menuBurger.style.display === "none"){
-        menuBurger.style.display = "block"; 
-        document.body.style.paddingLeft= "220px";
-    }else if (menuBurger.style.display === "block"){
+function displayBurger() {
+    if (menuBurger.style.display === "none") {
+        menuBurger.style.display = "block";
+        document.body.style.paddingLeft = "220px";
+    } else if (menuBurger.style.display === "block") {
         menuBurger.style.display = "none";
-        document.body.style.paddingLeft ="0";
+        document.body.style.paddingLeft = "0";
     }
 };
 
-getBtn.addEventListener("click",displayBurger);
+getBtn.addEventListener("click", displayBurger);
 
-/*
-//function navbar scroll
-window.onscroll= function(){
-    var latest = document.getElementById('latestCtn');
-    var navbar = document.getElementById(''); // à créer, et lui mettre display none par defaut.
-   
-    if(window.scrollY >= latest.offsetTop){
-    //quand la section latest arrive en haut de l'ecran !.
-      navbar.style.display = "block";
-    }else{
-      navbar.style.display= "none";
+// fin de fonction burger
+
+// fonction display block social
+
+var shares = document.getElementsByClassName("share");
+
+for (var i = 0; i <= shares.length - 1; i++) {
+    shares[i].addEventListener("click", displaySocial);
+}
+
+var socialNav = document.getElementById("socialNav");
+var blockCloseNav = document.getElementById("blockCloseNav");
+
+function displaySocial() {
+
+    if (socialNav.style.display === "none") {
+        socialNav.style.display = "flex";
+        blockCloseNav.style.display = "block";
+    } else {
+        socialNav.style.display = "none";
+        blockCloseNav.style.display = "none";
     }
-  };
-*/
+
+}
+// fin de fontion display block social
+
+blockCloseNav.addEventListener("click", function () {
+    if (socialNav.style.display === "flex") {
+        socialNav.style.display = "none";
+        blockCloseNav.style.display = "none";
+    }
+})
+
+//function navbar scroll
+
+var latest = document.getElementById('repairY');
+var navbar = document.getElementById('menuScroll');
+navbar.style.display = "none";
+
+window.onscroll = function () {
+
+    if (window.scrollY >= latest.offsetTop) {
+
+        if (window.innerWidth > 810) {
+            navbar.style.display = "flex";
+
+        } else if (window.innerWidth < 810) {
+            barMenu.style.marginTop = "0";
+        } else {
+            navbar.style.display = "none";
+        }
+    } else {
+        navbar.style.display = "none";
+        barMenu.style.marginTop = "20px";
+    }
+};
+
+// fin de fonction navbar scroll
